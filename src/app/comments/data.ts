51433aa6c3493e66894108333b95b0b632c6e703
chauhan-ankit24 <91563712+chauhan-ankit24@users.noreply.github.com>
2024-04-12 16:11:29 +0530

    routing done

---
+++ src/app/comments/data.ts
@@ -0,0 +1,10 @@
+export const comments = [
+  {
+    id: 1,
+    text: "This is a comment",
+  },
+  {
+    id: 2,
+    text: "This is another comment",
+  },
+];
---
+++ src/app/comments/route.ts
@@ -0,0 +1,22 @@
+import { NextRequest } from "next/server";
+import { comments } from "./data";
+
+export async function GET(request: NextRequest) {
+  const searchParams = request.nextUrl.searchParams;
+  const query = searchParams.get("query");
+  const filteredComments = query
+    ? comments.filter((comment) => comment.text.includes(query))
+    : comments;
+  return Response.json(filteredComments);
+}
+
+export async function POST(request: Request) {
+  const comment = await request.json();
+  const id = comments.length + 1;
+  const newComment = {
+    id: id,
+    text: comment.text,
+  };
+  comments.push(newComment);
+  return Response.json(comments, { status: 201 });
+}
